9912a648c2373391c7480927b2f541ee39443250
Gabriel-Labariento <gabrielmatthew.labariento@gmail.com>
2025-01-02 17:48:51 +0800

    Finished displayBook function.

--- script.js
+++ script.js
@@ -19,4 +19,32 @@ addBookToLibrary(book1);
 addBookToLibrary(book2);
 addBookToLibrary(book3);
 
-console.log(myLibrary)
+function displayBook(){
+    let container = document.querySelector(".container");
+
+    for (book of myLibrary){
+        let card = document.createElement("div");
+        card.setAttribute("class", "card");
+
+
+        let titleContainer = document.createElement("div");
+        let authorContainer = document.createElement("div");
+        let pagesContainer = document.createElement("div");
+        let readContainer = document.createElement("div");
+
+        titleContainer.textContent = book.title;
+        titleContainer.style.fontSize = "1.2rem";
+        titleContainer.style.fontWeight = "bold";
+        authorContainer.textContent = book.author;
+        pagesContainer.textContent = `${book.pages} pages` ;
+        readContainer.textContent = (book.hasRead) ? "You have read this book." : "You have not read this book yet.";
+
+        card.appendChild(titleContainer);
+        card.appendChild(authorContainer);
+        card.appendChild(pagesContainer);
+        card.appendChild(readContainer);
+        container.appendChild(card);
+    }
+}
+
+displayBook();
