1559e28ed3402a75fed5fd12207e785af7d41175
Gabriel-Labariento <gabrielmatthew.labariento@gmail.com>
2025-01-03 21:03:04 +0800

    Finished add book and remove book functions.

--- script.js
+++ script.js
@@ -15,14 +15,6 @@ function addBookToLibrary(book){
     myLibrary.push(book);
 }
 
-let book1 = new Book("Pride and Prejudice", "Jane Austen", 279, false);
-let book2 = new Book("1984", "George Orwell", 328, true);
-let book3 = new Book("To Kill a Mockingbird", "Harper Lee", 281, false);
-
-addBookToLibrary(book1);
-addBookToLibrary(book2);
-addBookToLibrary(book3);
-
 function displayBooks(){
     let container = document.querySelector(".container");
     
@@ -33,12 +25,18 @@ function displayBooks(){
     for (book of myLibrary){
         let card = document.createElement("div");
         card.setAttribute("class", "card");
-
+        card.dataset.bookNumber = myLibrary.indexOf(book);
 
         let titleContainer = document.createElement("div");
         let authorContainer = document.createElement("div");
         let pagesContainer = document.createElement("div");
         let readContainer = document.createElement("div");
+        let removeContainer = document.createElement("div");
+        let removeBtn = document.createElement("button");
+        removeBtn.setAttribute("class", "remove-btn");
+        removeBtn.dataset.bookNumber = myLibrary.indexOf(book);
+        removeBtn.textContent = "Remove";
+        
 
         titleContainer.textContent = book.title;
         titleContainer.style.fontSize = "1.2rem";
@@ -46,12 +44,20 @@ function displayBooks(){
         authorContainer.textContent = book.author;
         pagesContainer.textContent = `${book.pages} pages` ;
         readContainer.textContent = (book.hasRead) ? "You have read this book." : "You have not read this book yet.";
-
+        
+        removeContainer.appendChild(removeBtn);
         card.appendChild(titleContainer);
         card.appendChild(authorContainer);
         card.appendChild(pagesContainer);
         card.appendChild(readContainer);
+        card.appendChild(removeContainer);
         container.appendChild(card);
+
+        removeBtn.addEventListener("click", () => {
+            let index = removeBtn.dataset.bookNumber;
+            myLibrary.splice(index, 1);
+            displayBooks();
+        })
     }
 }
 
@@ -76,6 +82,9 @@ submitBtn.addEventListener("click", (event) => {
     dialog.close();
     addBookToLibrary(newBook);
     displayBooks();
+    newBookForm.reset();
 })
+
+const removeButtons = Array.from(document.querySelectorAll(".remove-btn"));
 
 
